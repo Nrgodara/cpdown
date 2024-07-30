65c81cf633cbcd0a33fc9871f6a3d67dae6c67f0
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-07-30 06:10:10 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,7 @@
-api_id = 25218674
-api_hash = "87c231f0e8704795b8239d06965b4351"
-bot_token = "7198600769:AAEA_EbrLFOYhMBm7pRi7BWKQgYHFdiIeTg"
-auth_users = [999741495]
-sudo_user = 1713924419
-log_channel = -1002236425890
-txt_channel = -1002236425890
+api_id = 29337172
+api_hash = "34b3a05a2038e77119115c6ee1ec9d56"
+bot_token = "6491187222:AAFiquALwXBmagVm2TS4WqCNiouUPCadr4U"
+auth_users = [1280494242]
+sudo_user = 1280494242
+log_channel = -1002021515776
+txt_channel = -1002021515776
